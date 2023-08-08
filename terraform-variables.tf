7604012941f0577b41eac1f9aadcbb0ff38ca152
Azeez <azeezjinadt@gmail.com>
2023-08-08 17:36:55 +0100

    Update .gitignore and untrack files

--- terraform-variables.tf
+++
@@ -1,6 +0,0 @@
-variable "florida_instance_type" {
-  type = string
-  default = "t2.micro"
-
-  
-}
--- userdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir /root/dir5
-
-sudo apt-get update
-sudo apt-get install zip -y
